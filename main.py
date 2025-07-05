6c388ce29fcad54eae77c67f586422e0f0524b74
Filip <filip.nilsson16@hotmail.com>
2025-07-05 17:15:15 +0200

    Add routers for recipes, cleanrooms, and substrates to main application

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.routers import recipes, cleanrooms, substrates
 
 app = FastAPI()
+
+app.include_router(recipes.router)
+app.include_router(cleanrooms.router)
+app.include_router(substrates.router)
 
 @app.get("/")
 def read_root():

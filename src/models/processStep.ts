7d3d7fdc9ef1a8cf986d3e0316a510d3c3754c70
Filip <filip.nilsson16@hotmail.com>
2025-07-05 20:22:14 +0200

    Removed

--- src/models/processStep.ts
+++
@@ -1,9 +0,0 @@
-// Represents a group of steps within a process recipe
-import { Step } from './step';
-
-export interface ProcessStep {
-    id: string;
-    name: string;
-    description?: string;
-    steps: Step[];
-}
